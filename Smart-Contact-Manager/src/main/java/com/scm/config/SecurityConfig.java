d194162378c8ace67abb70cda1049d69720ddf98
Jay Gupta <guptajay2510@gmail.com>
2024-06-23 14:03:59 +0530

    Created the login page

--- Smart-Contact-Manager/src/main/java/com/scm/config/SecurityConfig.java
+++ Smart-Contact-Manager/src/main/java/com/scm/config/SecurityConfig.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,47 +18,67 @@ public class SecurityConfig {
     // @Bean
     // public UserDetailsService userDetailsService() {
 
-    //     // default User
-    //     UserDetails user1 = User.withDefaultPasswordEncoder().
-    //             username("admin123").password("admin").roles("ADMIN", "USER").build();
-    //     UserDetails user2 = User.withDefaultPasswordEncoder()
-    //             .username("user123").password("user").roles("USER").build();
+    // // default User
+    // UserDetails user1 = User.withDefaultPasswordEncoder().
+    // username("admin123").password("admin").roles("ADMIN", "USER").build();
+    // UserDetails user2 = User.withDefaultPasswordEncoder()
+    // .username("user123").password("user").roles("USER").build();
 
-    //     InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager(user1, user2);
-    //     return inMemoryUserDetailsManager;
+    // InMemoryUserDetailsManager inMemoryUserDetailsManager = new
+    // InMemoryUserDetailsManager(user1, user2);
+    // return inMemoryUserDetailsManager;
     // }
 
     @Autowired
     private CustomUserDetailService userDetailService;
 
-    //CONFIGURATION OF AUTHENCIATION PROVIDER
+    // CONFIGURATION OF AUTHENCIATION PROVIDER
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        //object of user detail service
+        // object of user detail service
         daoAuthenticationProvider.setUserDetailsService(userDetailService);
-        //object of password encoder
+        // object of password encoder
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
         return daoAuthenticationProvider;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        //configure security of urls to allow and disallow access to authorized users
-        http.authorizeHttpRequests(authorize-> {
+        // configure security of urls to allow and disallow access to authorized users
+        http.authorizeHttpRequests(authorize -> {
             // authorize.requestMatchers("/home","/signup").permitAll();
             authorize.requestMatchers("/user/**").authenticated();
             authorize.anyRequest().permitAll();
         });
 
-        // default form loginpage
-        http.formLogin(Customizer.withDefaults());
 
+        // default form loginpage
+        http.formLogin(formLogin -> {
+            // redirect to this url
+            formLogin.loginPage("/login");
+            // validate details on this page
+            formLogin.loginProcessingUrl("/authenticate");
+            // after successful login, redirect to this page
+            formLogin.successForwardUrl("/user/profile");
+            // after failed login, redirect to this page
+            //formLogin.failureForwardUrl("/login?error=true");
+            // this is the username parameter to look for when authenticating user
+            formLogin.usernameParameter("email");
+            // this is the password parameter to look for when authenticating user
+            formLogin.passwordParameter("password");
+        });
+
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.logout(logout -> {
+            logout.logoutUrl("/do-logout");
+            logout.logoutSuccessUrl("/login?logout=true");
+        });
         return http.build();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
